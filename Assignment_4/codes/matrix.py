24e96f4aae4fdc2b93ef8684d12d462d237701b9
hemanthreddy6 <127115476+hemanthreddy6@users.noreply.github.com>
2023-05-19 07:39:41 +0530

    Add files via upload

---
+++ Assignment_4/codes/matrix.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# Formatting the print options
+np.set_printoptions(formatter={'float': lambda x: "{0:0.4f}".format(x)})
+
+# Initializing the matrix
+A = np.array([[5/6,1/6],
+              [0,0],])
+          
+# Getting A^2
+B = np.linalg.matrix_power(A,2)
+print(B)
+
+print("------------------------------")
+
+# Getting A^3
+B = np.linalg.matrix_power(A,3)
+print(B)
